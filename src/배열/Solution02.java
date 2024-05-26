b8fbaa02e24b2bc0b95a92b1dcc3748efd53fc1e
xyz-wr <pponi13468@naver.com>
2024-05-27 01:50:22 +0900

    배열 제어하기

---
+++ src/배열/Solution02.java
@@ -0,0 +1,20 @@
+package 배열;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution02 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{4, 2, 2, 1, 3, 4})));
+        System.out.println(Arrays.toString(solution(new int[]{2, 1, 1, 3, 2, 5, 4})));
+    }
+
+    // 이 부분을 변경해서 실행해보세요.
+    private static int[] solution(int[] arr) {
+        Integer[] result = Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new);
+        Arrays.sort(result, Collections.reverseOrder());
+        return Arrays.stream(result).mapToInt(Integer::intValue).toArray();
+    }
+
+}
