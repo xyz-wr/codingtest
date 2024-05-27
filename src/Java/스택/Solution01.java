303838bf87ab4b0690b6545a51392e3415177c13
xyz-wr <pponi13468@naver.com>
2024-05-27 21:09:44 +0900

    올바른 괄호

---
+++ src/Java/스택/Solution01.java
@@ -0,0 +1,21 @@
+package Java.스택;
+
+import java.util.*;
+class Solution01 {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        char[] ch = s.toCharArray();
+        for (char c : ch) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || stack.pop() == c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
