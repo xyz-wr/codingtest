448f54004038e82f55e4cb3a6b90e441c5ef326f
xyz-wr <pponi13468@naver.com>
2024-05-31 18:49:04 +0900

    요세푸스 문제

---
+++ src/Java/큐/Solution01.java
@@ -0,0 +1,29 @@
+package Java.큐;
+
+import java.util.ArrayDeque;
+
+public class Solution01 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(5, 2));
+    }
+
+    // 이 부분을 변경해서 실행해보세요.
+    private static int solution(int N, int K) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 1; i <= N; i++) {
+            deque.addLast(i);
+        }
+
+        while (deque.size() > 1) {
+            for (int i = 0; i < K-1; i++) {
+                deque.addLast(deque.pollFirst());
+            }
+            deque.pollFirst();
+        }
+
+        return deque.pollFirst();
+    }
+
+}
