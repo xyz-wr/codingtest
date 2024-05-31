4bc53abb1e45d668011c796be121d90e8c5af22a
xyz-wr <pponi13468@naver.com>
2024-05-31 21:25:17 +0900

    기능 개발

---
+++ src/Java/큐/Solution02.java
@@ -0,0 +1,31 @@
+package Java.큐;
+
+import java.util.*;
+
+class Solution02 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> answer = new ArrayDeque<>();
+        int n = progresses.length;
+        int[] daysLeft = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            daysLeft[i] = (int) Math.ceil((100.0 - progresses[i])/speeds[i]);
+        }
+
+        int cnt = 0;
+        int maxDay = daysLeft[0];
+        for (int i = 0; i < n; i++) {
+            if (daysLeft[i] <= maxDay) {
+                cnt++;
+            } else {
+                answer.add(cnt);
+                cnt = 1;
+                maxDay = daysLeft[i];
+            }
+        }
+
+        answer.add(cnt);
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+}
