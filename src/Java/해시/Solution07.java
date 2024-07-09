eb9a01e09dfac0ea2587f95976d147ddde0b9b4b
xyz-wr <pponi13468@naver.com>
2024-07-09 19:16:47 +0900

    의상

---
+++ src/Java/해시/Solution07.java
@@ -0,0 +1,18 @@
+package Java.해시;
+/** 의상 */
+import java.util.*;
+class Solution07 {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        }
+
+        int answer = 1;
+        for (String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
+        }
+
+        return answer - 1;
+    }
+}
