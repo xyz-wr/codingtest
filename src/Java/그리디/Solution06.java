a37cc0e6c197c5131b97a02f304010fc0c858c71
xyz-wr <pponi13468@naver.com>
2024-07-11 18:08:14 +0900

    큰 수 만들기

---
+++ src/Java/그리디/Solution06.java
@@ -0,0 +1,22 @@
+package Java.그리디;
+
+import java.util.Stack;
+/** 큰 수 만들기 */
+public class Solution06 {
+    public String solution(String number, int k) {
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
+    }
+}
