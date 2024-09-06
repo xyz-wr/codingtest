459b8a12807704cef91a7f6a769709b6f9d455bd
xyz-wr <pponi13468@naver.com>
2024-09-06 14:30:10 +0900

    연속 부분 수열의 합

---
+++ src/Java/프로그래머스/Solution05.java
@@ -0,0 +1,19 @@
+package Java.프로그래머스;
+
+import java.util.*;
+class Solution05 {
+    public int solution(int[] elements) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 1; i <= elements.length; i++) {
+            for (int j = 0; j < elements.length; j++) {
+                int sum = 0;
+                for (int k = j; k < i + j; k++) {
+                    sum += elements[k % elements.length];
+                }
+                set.add(sum);
+            }
+        }
+        return set.size();
+    }
+}
